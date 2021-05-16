04aea6075672b1bf5aee3942089cb61c5e94f3dd
yuki-boy <issi0430bjc@gmail.com>
2021-05-16 09:41:38 +0900

    publicにcss/index.cssを追加。postcreateとcommentcreateにcssを当てた。

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
 </head>
 <body>
--- resources/views/posts/postcreate.blade.php
+++ resources/views/posts/postcreate.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.layout')
 @section('content')
 
-<form method="post" action="{{ route('posts.save') }}">
-@csrf
-  <div>
-  <textarea name="body" cols="30" rows="10" placeholder="140字以内で投稿して下さい">{{ old('body') }}</textarea><br>
+<div class="postcreate">
+  <form method="post" action="{{ route('posts.save') }}">
+  @csrf
+    <div>
+    <textarea name="body" cols="30" rows="10" placeholder="140字以内で投稿して下さい">{{ old('body') }}</textarea><br>
 
-  <input type="submit" value="投稿">
-  </div>
-</form>
+    <input type="submit" value="投稿">
+    </div>
+  </form>
+</div>
 
 
 
