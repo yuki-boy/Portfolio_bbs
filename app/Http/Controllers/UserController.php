edc6c30e0bb1b3957527cd7cf01560e7b7a93015
yuki-boy <issi0430bjc@gmail.com>
2021-05-17 14:46:09 +0900

    UserControllerの中身をUserRepositoryにコンポーネント化

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,16 +6,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 use App\Models\User;
+use App\Repositories\UserRepository;
 use DB;
 
 class UserController extends Controller
 {
     public function Mypage($user_id)
     {
-		$myPages = Post::select('body')
-		->where('user_id','=',Auth::id())
-		->orderBy('posts.id', 'desc')
-		->get();
+		$myPages = UserRepository::getCurrentUser();
 
         return view('posts.mypage', compact('myPages'));
     }
