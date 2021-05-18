7eda02b6df482665f701d08bee8790b78d8069df
yuki-boy <issi0430bjc@gmail.com>
2021-05-18 12:19:11 +0900

    CommentControllerの内容を一部CommentRepositoryにコンポーネント化

---
+++ app/Repositories/CommentRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Comment;
+
+class CommentRepository
+{
+  public static function deleteComment($comment_id)
+  {
+    $deleteComment = Comment::find($comment_id);
+    
+    return $deleteComment->delete();
+  }
+
+
+
+}
+
+
+?>
