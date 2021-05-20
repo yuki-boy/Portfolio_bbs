b5553ae971542f7cbe85b7ddf73cf1be7e0a811a
yuki-boy <issi0430bjc@gmail.com>
2021-05-20 14:38:12 +0900

    jQuery直った

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  //queue()で処理を溜めてdequeue()で実行。3秒経ったらfadeOut()
+  //queue()で処理を溜めてdequeue()で実行。1秒経ったらfadeOut()
  $("#timeout").fadeIn().queue(function() {
      setTimeout(function(){$("#timeout").dequeue();
      }, 1000);
