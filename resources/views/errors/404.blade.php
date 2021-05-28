7f36e969a5ba103d36ce7778e3359d0de87fd2e7
yuki-boy <issi0430bjc@gmail.com>
2021-05-28 16:41:14 +0900

    logout後にloginページに遷移するよう実装

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -46,7 +46,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title">
-                Sorry, the page you are looking for could not be found.
+                404
             </div>
         </div>
     </div>
